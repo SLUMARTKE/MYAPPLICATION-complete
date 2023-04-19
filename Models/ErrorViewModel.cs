d3a127cf70bc88879494e73723a87b17cbfb8458
Admin <Admin@DESKTOP-EQ55Q8H>
2023-04-19 00:53:25 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MYAPPLICATION.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
